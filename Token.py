f0bd9f0d9c1866893dcf5c5417f687d73e455a2a
Pyeon Dong Hyuk <lop1322@ajou.ac.kr>
2021-12-02 16:22:36 +0900

    Edit File name according to the purpose

--- Token.py
+++ Token.py
@@ -7,7 +7,7 @@ np.set_printoptions(precision=2, linewidth=120)
 
 def replace_words(text):
     words = ' '.join(re.sub("(@[A-Za-z0-9]+)|(#[A-Za-z0-9]+)", " ", text).split())
-    words = ' '.join(re.sub("[\.\,\!\?\:\;\-\=]", " ", words).split())
+    words = ' '.join(re.sub("[\.\,\!\?\:\;\-\=\#\/]", " ", words).split())
     words = words.replace("RT"," ") ##RT란 단어는 필요가 없다고 판단.
     words = words.lower()
     words = words.split()
@@ -18,5 +18,10 @@ dataset = pd.read_json('./Biden.json', lines = True)
 
 dataset['words'] = dataset['text'].apply(replace_words)
 
+output_file = "Biden_words.json"
 
-print(dataset['words'])
+## 분리하고 가공화한 데이터를 print한다. 이후 data를 json file로 저장한다.
+print(dataset['words']) 
+with open(output_file, 'w', encoding='utf-8') as f:
+    for line in dataset['words']:
+        f.write(str(line) + '\n')
